2a0cbd55fe844a21d00eafb1cb91d3a6e49ae521
unknown <kamatheaomkar@gmail.com>
2022-11-03 11:22:56 +0530

    added Address.java

---
+++ Address.java
@@ -0,0 +1,34 @@
+package com.velocity.Association;
+
+public class Address {
+	private int streetNo;
+	private String city;
+	private String pincode;
+	private String state;
+	
+	public int getStreetNo() {
+		return streetNo;
+	}
+	public void setStreetNo(int streetNo) {
+		this.streetNo = streetNo;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+}
